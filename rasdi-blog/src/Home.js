4451525c876553ccfd7a899e1a64710e934e9195
rasdi28 <rasdi.abdulrohman15120@student.unsika.ac.id>
2020-12-29 22:32:16 +0700

    css sytle and click event

--- rasdi-blog/src/Home.js
+++ rasdi-blog/src/Home.js
@@ -1,8 +1,22 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        alert("Hello, Ngoding Bareng.id",e);
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('Hello ' + name);
+
+    }
+
+
     return (
         <div className = "home">
             <h2> This is Home</h2>
             <h2> Hello World</h2>
+            <button onClick= {handleClick}>Click Me</button>
+            <button onClick={(e)=>handleClickAgain('rasdi')
+            } > Click Me Again</button>
         </div>
       );
 }
