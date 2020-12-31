e8950486838d7c3eb58f584c3b086f40c36b7234
rasdi28 <rasdi.abdulrohman15120@student.unsika.ac.id>
2020-12-31 07:40:04 +0700

    react

--- rasdi-blog/src/Home.js
+++ rasdi-blog/src/Home.js
@@ -1,18 +1,26 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [name,SetName] = useState('rasdi');
+    const [age, setAge] = useState (20);
 
     const handleClick = (e) => {
         alert("Hello, Ngoding Bareng.id",e);
     }
 
-    const handleClickAgain = (name) => {
-        console.log('Hello ' + name);
+    const handleClickAgain = () => {
+       SetName ('rohman');
+       setAge (25);
 
     }
+
+
 
     return (
         <div className = "home">
             <h2> This is Home</h2>
             <h2> Hello World</h2>
+            <p>{ name } {age} Years</p>
             <button onClick= {handleClick}>Click Me</button>
             <button onClick={(e)=>handleClickAgain('rasdi')
             } > Click Me Again</button>
