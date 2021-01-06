e857f329c1cd7ab6399ae6c2881771799e598b36
rasdi28 <rasdi.abdulrohman15120@student.unsika.ac.id>
2021-01-06 21:30:50 +0700

    change css navbar

--- rasdi-blog/src/Navbar.js
+++ rasdi-blog/src/Navbar.js
@@ -1,8 +1,6 @@
 const Navbar = () => {
     return ( 
        <nav className = "navbar">
-           <img src = "/images/logo1.png" alt= "logo "></img>
-           <h1></h1>
            <div className = "links">
                <a href = "/">Home</a>
                <a href="/create" >New Blog</a>
